f34a327e51ae7dac1e3707d55a2376693c3e53b6
lcsouza2 <lscouzagarcia@gmail.com>
2026-06-30 20:43:13 -0300

    feat: rename interface.c to user_interface.c

--- src/user_interface.c
+++ src/user_interface.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
 #include "memtable.h"
+#include "logging.h"
+#include "settings.h"
+#include "wal.h"
+#include "user_interface.h"
+#include "sstables.h"
+
+void db_insert(Database *db, const char *key, const char *value) {
+    append_wal_log(key, value);
+
+    Memtable *tree = insert_memtable(db->memtable, (char *)key, (char *)value);
+    db->memtable = tree;
+
+    if (value) info("Client INSERT: %s -> %s", key, value);
+    else info("Client DELETE: %s", key);
+
+    if (tree->bytes_allocated > MAX_MEMTABLE_SIZE) {
+        info("Memtable threshold exceeded. Flushing...");
+        if (flush_memtable_to_disk(tree, 0) == 0) {
+            clear_memtable(tree);
+            clear_wal_logs();
+        }
+    }
+}
+
+void init_database(Database *db) {
+    db->memtable = create_memtable();
+    if (!db->memtable) {
+        error("Failed to initialize memtable.");
+        return;
+    }
+    sync_wal_to_memtable(db->memtable);
+}
+
 
 void clear() {
     printf("\033[H\033[J");
 }
 
 void print_main_menu() {
-    clear();
     printf("=== Main Menu ===\n");
     printf("1. Add Key-Value Pair\n");
     printf("2. Get Value by Key\n");
@@ -19,13 +51,18 @@ void print_main_menu() {
 
 
 int main() {
+    Database *db = malloc(sizeof(Database));
+    init_database(db);
+
     while (1) {
         print_main_menu();
         int choice;
         scanf("%d", &choice);
         switch (choice) {
             case 1:
-                // Add Key-Value Pair
+                char key[MAX_KEY_LENGTH];
+                char value[1];
+                db_put(db, key, value);
                 break;
             case 2:
                 // Get Value by Key
