59923383bc4b17cb5f2d42d4e50112e94eb21e30
lcsouza2 <lscouzagarcia@gmail.com>
2026-06-30 10:34:20 -0300

    tests: add test to memtable flushing

--- tests/unit/test_sstables.c
+++ tests/unit/test_sstables.c
@@ -3,22 +3,26 @@
 #include "test_utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int test_create_sstable_success() {
-    SSTable *sstable = create_sstable_in_memory("Apple", "Red", 0);
+int test_flush_memtable_to_disk_success() {
+    Memtable *memtable = create_memtable();
+    insert_memtable(memtable, "key1", "value1");
+    insert_memtable(memtable, "key2", "value2");
+    insert_memtable(memtable, "key3", "value3");
 
-    ASSERT_TEST(sstable != NULL, "Struct returned NULL");
-    ASSERT_TEST(strcmp(sstable->min_key, "Apple") == 0, "Wrong min_key assigned");
-    ASSERT_TEST(strcmp(sstable->max_key, "Red") == 0, "Wrong max_key assigned");
-    ASSERT_TEST(sstable->level == 0, "Wrong level assigned");
+    SSTable *sstable = flush_memtable_to_disk(memtable, 0);
 
-    FILE *fp = fopen(sstable->path, "rb");
-    ASSERT_TEST(fp != NULL, "File was not created on disk");
-    if (fp) fclose(fp);
+    ASSERT_TEST(sstable != NULL, "Failed to flush memtable to disk.");
+    ASSERT_TEST(sstable->level == 0, "SSTable level should be 0.");
+    ASSERT_TEST(strcmp(sstable->min_key, "key1") == 0, "SSTable min_key should be 'key1'.");
+    ASSERT_TEST(strcmp(sstable->max_key, "key3") == 0, "SSTable max_key should be 'key3'.");
 
+    // Clean up
     free(sstable->path);
     free(sstable);
+    free_memtable(memtable);
 
-    info("test_create_sstable_success passed.");
+    info("test_flush_memtable_to_disk_success passed.");
     return 0;
 }
